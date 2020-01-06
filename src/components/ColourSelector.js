f486dfe25f063a0c6f801cb6dab9b657154c98c2
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-01-07 10:38:43 +1100

    class

---
+++ src/components/ColourSelector.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ColourSelector extends Component {
+    render() {
+        return (
+            <input type="color"  />
+        );
+    }
+}
+
+export default ColourSelector;
