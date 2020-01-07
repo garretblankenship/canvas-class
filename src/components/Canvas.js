d888c0c5558a8db8db6d1559a38d68fb56e366d4
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-01-07 16:29:30 +1100

    class

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,11 +2,28 @@ import React, { Component } from "react";
 import ColourSelector from "./ColourSelector";
 
 class Canvas extends Component {
-    state = { hex: "#ffffff" }
+    state = { 
+        hex: "#ffffff",
+        coords: null,
+        height: 400,
+        width: 400
+    }
 
     constructor(props) {
         super(props);
         this.canvasRef = React.createRef();
+        this.context = null;
+    }
+
+    componentDidUpdate() {
+        this.setContext();
+    }
+
+    setContext() {
+        this.context = this.canvasRef.current.getContext("2d");
+        this.context.strokeStyle = this.state.hex;
+        this.context.lineJoin = "round";
+        this.context.lineWidth = 3;
     }
 
     componentDidMount() {
@@ -17,8 +34,38 @@ class Canvas extends Component {
         this.setState({ hex: event.target.value });
     }
 
+    onCanvasMouseDown = (event) => {
+        const x = event.nativeEvent.offsetX;
+        const y = event.nativeEvent.offsetY;
+        this.setState({ coords: [x, y] });
+    }
+
+    onCanvasMouseUp = (event) => {
+        this.setState({ coords: null });
+    }
+
+    onCanvasMouseMove = (event) => {
+        const x = event.nativeEvent.offsetX;
+        const y = event.nativeEvent.offsetY;
+        const { coords, height, width } = this.state;
+
+        if (x > 0 && x < width && y > 0 && y < height) {
+            if (coords) {
+                this.context.beginPath();
+                this.context.moveTo(coords[0], coords[1]);
+                this.context.lineTo(x,y);
+                this.context.closePath();
+                this.context.stroke();
+                this.setState({ coords: [x, y]});
+            }
+        } else {
+            this.setState({ coords: null });
+        }
+    }
+
+
     render() {
-        const { hex } = this.state;
+        const { hex, height, width } = this.state;
 
         return (
             <div>
@@ -29,10 +76,13 @@ class Canvas extends Component {
                     />
                 </div>
                 <canvas
-                    width="400"
-                    height="400"
+                    width={width}
+                    height={height}
                     style={{ border: "6px solid black" }}
                     ref={this.canvasRef}
+                    onMouseMove={this.onCanvasMouseMove}
+                    onMouseDown={this.onCanvasMouseDown}
+                    onMouseUp={this.onCanvasMouseUp}
                 />
             </div>
         );
