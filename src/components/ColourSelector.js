e7adf247dd4e9addbdf97158b5db2accea970fab
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-01-07 11:57:10 +1100

    class

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 class ColourSelector extends Component {
+    state = { 
+        hex: this.props.hex, 
+        name: ""
+    }
+    
+    onInputChange(event) {
+        this.setState({ hex: event.target.value });
+    }
+    
     render() {
-        const { hex } = this.props;
+        const { hex } = this.state;
 
         return (
-            <input type="color" defaultValue={hex}  />
+            <>
+                <input type="color" value={hex} onChange={this.onInputChange}  />
+            </>
         );
     }
 
